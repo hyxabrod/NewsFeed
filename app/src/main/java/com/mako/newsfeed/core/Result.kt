29d21a602c7104b38da6f13511ed35b4f81981b9
hyxabrod <hyxabrod@gmail.com>
2026-01-09 15:46:05 +0200

    Delete unused class

--- app/src/main/java/com/mako/newsfeed/core/Result.kt
+++
@@ -1,6 +0,0 @@
-package com.mako.newsfeed.core
-
-sealed interface Result<out T> {
-    data class Success<T>(val data: T) : Result<T>
-    data class Error(val exception: Throwable) : Result<Nothing>
-}
