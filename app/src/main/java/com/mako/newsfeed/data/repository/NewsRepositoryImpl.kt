de6c0bde4a3bc2cadc58a3c9e3b12b02aaf10743
hyxabrod <hyxabrod@gmail.com>
2026-01-09 13:32:24 +0200

    Code clean up

--- app/src/main/java/com/mako/newsfeed/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/mako/newsfeed/data/repository/NewsRepositoryImpl.kt
@@ -24,60 +24,58 @@ import kotlin.collections.map
 class NewsRepositoryImpl
 @Inject
 constructor(private val client: HttpClient, private val dao: ArticlesDao) : NewsRepository {
-    override suspend fun getHeadlines(
-        page: Int,
-        isTopNews: Boolean
-    ): List<NewsDomainEntity> {
+    override suspend fun getHeadlines(page: Int, isTopNews: Boolean): List<NewsDomainEntity> {
         val response = fetchHeadlines(page, isTopNews)
-        val articles = response.articles.map { dto ->
-            ArticleDomainEntity(
-                uuid = UUID.nameUUIDFromBytes(dto.url.toByteArray()).toString(),
-                sourceName = dto.source?.name ?: "",
-                title = dto.title,
-                urlToImage = dto.urlToImage ?: "",
-                publishedAt = dto.publishedAt,
-                author = dto.author ?: "",
-                content = dto.content ?: "",
-                description = dto.description ?: "",
-                url = dto.url
-            )
-        }
+        val articles =
+                response.articles.map { dto ->
+                    ArticleDomainEntity(
+                            uuid = UUID.nameUUIDFromBytes(dto.url.toByteArray()).toString(),
+                            sourceName = dto.source?.name ?: "",
+                            title = dto.title,
+                            urlToImage = dto.urlToImage ?: "",
+                            publishedAt = dto.publishedAt,
+                            author = dto.author ?: "",
+                            content = dto.content ?: "",
+                            description = dto.description ?: "",
+                            url = dto.url
+                    )
+                }
         saveHeadlines(articles)
 
         return articles.map { news ->
             NewsDomainEntity(
-                uuid = news.uuid,
-                sourceName = news.sourceName,
-                title = news.title,
-                urlToImage = news.urlToImage,
-                publishedAt = news.publishedAt
+                    uuid = news.uuid,
+                    sourceName = news.sourceName,
+                    title = news.title,
+                    urlToImage = news.urlToImage,
+                    publishedAt = news.publishedAt
             )
         }
     }
 
     override suspend fun saveArticleToServer(article: ArticleDomainEntity): Boolean {
-        kotlinx.coroutines.delay(1000) // Simulate network delay
+        kotlinx.coroutines.delay(1000)
         val proto =
-            ArticleProto.newBuilder()
-                .setTitle(article.title)
-                .setUrl(article.url)
-                .setContent(article.content)
-                .build()
+                ArticleProto.newBuilder()
+                        .setTitle(article.title)
+                        .setUrl(article.url)
+                        .setContent(article.content)
+                        .build()
 
         return sendArticleProtobuf(proto)
     }
 
     private suspend fun fetchHeadlines(page: Int, isTopNews: Boolean): NewsResponse {
         val path =
-            if (isTopNews) NewsRepository.TOP_HEADERS_PATH else NewsRepository.EVERYTHING_PATH
-        val data = client
-            .get(path) {
-                if (isTopNews) parameter("country", "us")
-                if (!isTopNews) parameter("q", "USA")//
-                parameter("apiKey", BuildConfig.API_KEY)
-                parameter("pageSize", NewsRepository.PAGE_SIZE)
-                parameter("page", page)
-            }
+                if (isTopNews) NewsRepository.TOP_HEADERS_PATH else NewsRepository.EVERYTHING_PATH
+        val data =
+                client.get(path) {
+                    if (isTopNews) parameter("country", "us")
+                    if (!isTopNews) parameter("q", "USA")
+                    parameter("apiKey", BuildConfig.API_KEY)
+                    parameter("pageSize", NewsRepository.PAGE_SIZE)
+                    parameter("page", page)
+                }
         if (data.status == HttpStatusCode.UpgradeRequired) {
             throw UpgradeRequiredException()
         }
@@ -86,19 +84,19 @@ constructor(private val client: HttpClient, private val dao: ArticlesDao) : News
 
     override suspend fun saveHeadlines(articles: List<ArticleDomainEntity>) {
         val entities =
-            articles.map { domain ->
-                ArticleDataEntity(
-                    uuid = domain.uuid,
-                    sourceName = domain.sourceName,
-                    author = domain.author,
-                    title = domain.title,
-                    description = domain.description,
-                    url = domain.url,
-                    urlToImage = domain.urlToImage,
-                    publishedAt = domain.publishedAt,
-                    content = domain.content,
-                )
-            }
+                articles.map { domain ->
+                    ArticleDataEntity(
+                            uuid = domain.uuid,
+                            sourceName = domain.sourceName,
+                            author = domain.author,
+                            title = domain.title,
+                            description = domain.description,
+                            url = domain.url,
+                            urlToImage = domain.urlToImage,
+                            publishedAt = domain.publishedAt,
+                            content = domain.content,
+                    )
+                }
         dao.insertAll(entities)
     }
 
@@ -109,11 +107,11 @@ constructor(private val client: HttpClient, private val dao: ArticlesDao) : News
     override suspend fun getHeadlinesFromDb(): List<NewsDomainEntity> {
         return dao.getAllSync().map { news ->
             NewsDomainEntity(
-                uuid = news.uuid,
-                sourceName = news.sourceName,
-                title = news.title,
-                urlToImage = news.urlToImage,
-                publishedAt = news.publishedAt
+                    uuid = news.uuid,
+                    sourceName = news.sourceName,
+                    title = news.title,
+                    urlToImage = news.urlToImage,
+                    publishedAt = news.publishedAt
             )
         }
     }
@@ -138,8 +136,7 @@ constructor(private val client: HttpClient, private val dao: ArticlesDao) : News
 }
 
 suspend fun ArticleSaveServiceCoroutineStub.fakeSendArticle(
-    request: ArticleProto
+        request: ArticleProto
 ): ArticleSaveResponse {
     return ArticleSaveResponse.newBuilder().setSuccess(true).build()
 }
-
